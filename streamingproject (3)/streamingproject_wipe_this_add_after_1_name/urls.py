dc80520f69b09d9e0f9ec890dea5336692357df5
kremlev404 <kremlev404@gmail.com>
2020-02-21 12:37:19 +0300

    3

---
+++ streamingproject (3)/streamingproject_wipe_this_add_after_1_name/urls.py
@@ -0,0 +1,16 @@
+
+from django.conf.urls import url
+from django.contrib import admin
+
+from streamingproject import views
+
+# url patterns for browsing 
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^stream/(?P<num>\d+)/(?P<stream_path>(.*?))/$',views.dynamic_stream,name="dynamic_stream"),  
+    url(r'^stream/screen/$',views.indexscreen),
+    
+    url(r'^auth/.*/.*',views.auth),
+    url(r'^delauth/',views.del_auth)
+]
